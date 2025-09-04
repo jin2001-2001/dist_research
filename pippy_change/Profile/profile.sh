877e652b3f5d83a62e73475a6342cee271fc8acf
jinjianli <jin2001@jinjianlis-MacBook-Air-6.local>
2025-09-04 02:20:28 -0500

    profile

--- pippy_change/Profile/profile.sh
+++ pippy_change/Profile/profile.sh
@@ -41,7 +41,7 @@ for util in "${UTILS[@]}"; do
     # quota is microseconds of CPU time across *all* cores per period.
     # To target X% of full machine: quota = (X/100) * (CORES * PERIOD)
     QUOTA=$(( util * CORES * PERIOD_US / 100 ))
-    sudo bash -c "echo \"${QUOTA} ${PERIOD_US}\" > /sys/fs/cgroup/${CGROUP_NAME}/cpu.max"
+    printf "%d %d\n" "$quota" "$PERIOD" | sudo tee "${GROUP}/cpu.max" >/dev/null
     echo "Set cpu.max to ${QUOTA} ${PERIOD_US}"
 
 
