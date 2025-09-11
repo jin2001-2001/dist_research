d9ec05a544e477a1ce3467108e2d7e6fbd2e61cc
jinjianli <jin2001@jinjianlis-MacBook-Air-6.local>
2025-09-10 22:53:54 -0500

    exp running

--- pippy_change/D2Dexp_runner.py
+++ pippy_change/D2Dexp_runner.py
@@ -283,7 +283,7 @@ def main():
 
     raw = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
 
-    block = 512
+    block = 256
 
     def tok_fn(ex): 
         return tok(ex["text"], return_attention_mask=False)
