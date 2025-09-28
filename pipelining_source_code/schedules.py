e6c144c1ac335c7e35e29ceb944b7b83c70ccfb8
LinZiyang <119728387+LinZiyang666@users.noreply.github.com>
2025-09-28 15:17:36 -0500

    sdf

--- pipelining_source_code/schedules.py
+++ pipelining_source_code/schedules.py
@@ -802,7 +802,7 @@ class Schedule1F1B(PipelineScheduleSingle):
         self._rec = Recorder(rank=dist.get_rank(),net_sample_interval_ms=10)  
         self._rec.set_enabled(True)
         
-        
+        self.is_print = 0
         arg_mbs, kwarg_mbs = self._check_inputs(arg_mbs, kwarg_mbs, target_mbs, losses)
         
         
@@ -841,6 +841,21 @@ class Schedule1F1B(PipelineScheduleSingle):
                     {k: (v.shape if torch.is_tensor(v) else type(v))
                     for k, v in kwarg_mbs[fwd_mb_index]}
                 )
+                if self.is_print == 0:
+                    self.is_print =1
+                    from tensor_debug import dump_forward_debug
+                    import os
+                    try:
+                        save_dir = dump_forward_debug(
+                            save_root=os.path.abspath("./framework_forward"),
+                            composite_args=arg_mbs[fwd_mb_index],
+                            composite_kwargs=kwarg_mbs[fwd_mb_index],
+                            outputs=None,
+                            tag=f"rank{torch.distributed.get_rank() if torch.distributed.is_initialized() else 0}"
+                        )
+                        print(f"[forward-debug] saved to: {save_dir}")
+                    except Exception as e:
+                        print(f"[forward-debug] dump failed: {e}")
                 output = self._stage.forward_one_chunk(
                     fwd_mb_index, arg_mbs[fwd_mb_index], kwarg_mbs[fwd_mb_index]
                 )  # type: ignore[index]
