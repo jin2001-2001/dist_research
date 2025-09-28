df1b1067ef52b84ebf29474992049df03dd5f706
LinZiyang <119728387+LinZiyang666@users.noreply.github.com>
2025-09-28 15:28:13 -0500

    sdf

--- D2Dexp_runner.py
+++ D2Dexp_runner.py
@@ -323,7 +323,8 @@ def main():
         raise ValueError(f"Mbatch {total_batchs} not equal to {int(args.batch_size/args.microbatch_num)},misbatch plan's assumption")
     
     print(f"n_microbatches {args.batch_size}")
-    sched = PipelineScheduleRuntimeWithDirection([stage], n_microbatches=args.batch_size, loss_fn=loss_fn, root_pass=args.sudo_pass)
+    #sched = PipelineScheduleRuntimeWithDirection([stage], n_microbatches=args.batch_size, loss_fn=loss_fn, root_pass=args.sudo_pass)
+    sched = PipelineScheduleRuntimeWithDirection([stage], n_microbatches=20, loss_fn=loss_fn, root_pass=args.sudo_pass)
 
     # === Memory monitor: start & register containers (CPU/Gloo safe) ===
 
@@ -356,11 +357,11 @@ def main():
     
     
     batch_info,group_info = plan_batch_parser(args.plan_loc)
-    actions = generate_1f1b_pipeline_actions_pro(num_stages= total_stages, total_samples = args.batch_size, num_microbatches= args.microbatch_num,
-                                                 group_info=group_info, batch_info=batch_info,
-                                                  upstream = args.upstream)
+    # actions = generate_1f1b_pipeline_actions_pro(num_stages= total_stages, total_samples = args.batch_size, num_microbatches= args.microbatch_num,
+    #                                              group_info=group_info, batch_info=batch_info,
+    #                                               upstream = args.upstream)
     print(f"对应action {actions[dist.get_rank()]}")
-    # actions = generate_1f1b_pipeline_actions(num_stages= total_stages, num_microbatches= 20, upstream= 1000)
+    actions = generate_1f1b_pipeline_actions(num_stages= total_stages, num_microbatches= 20, upstream= 1000)
     
     sched._load_actions(actions, format="compute_comms")
     
